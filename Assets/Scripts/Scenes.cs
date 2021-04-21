ec2f2d34faedf8f0d481e79a2c16a986b6ed48b9
TheMemeMeister <60407375+TheMemeMeister@users.noreply.github.com>
2021-04-21 02:53:52 -0400

    More UI, Scenes

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Scenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        Debug.Log("test");
+        SceneManager.LoadScene(1);
+    }
+    public Button playbutton;
+    public Button quitbutton;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        playbutton.GetComponent<Button>().onClick.AddListener(PlayGame);
+        quitbutton.GetComponent<Button>().onClick.AddListener(Quit);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+}
