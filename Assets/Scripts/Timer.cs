4526878dd4987c342daeee4ff41f47391b8e2e3a
TheMemeMeister <60407375+TheMemeMeister@users.noreply.github.com>
2021-04-21 01:32:36 -0400

    Imported TMP, Made timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timeText;
+    public  float timeValue = 180; //going to start with seconds 
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0; 
+        }
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if(timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+        else if(timeToDisplay > 0)
+        {
+            timeToDisplay += 1; //shows the last half second
+        }
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = "Time Remaining:" + string.Format("{0:00}:{1:00}", minutes, seconds); //hope this works with TMP
+    }
+    
+}
