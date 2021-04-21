5a7845714db9cf86cd06ca781ca8feeeaeef83e0
TheMemeMeister <60407375+TheMemeMeister@users.noreply.github.com>
2021-04-20 21:28:11 -0400

    Fixed jump. Rotations are a little off, will come back to this later
    Had to redo it from ground up. New code is cleaner. Keeping old code for reference,

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,57 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class MovementController : MonoBehaviour
-{
+//public class MovementController : MonoBehaviour
+//{
 
-    public CharacterController controller;
-    public Transform cam;
-    public float speed = 5;
-    public float gravity = 9.81f;
-    private Rigidbody ninjaRB;
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
-    public float jumpForce = 10f;
-    public LayerMask groundLayers;
-    public CapsuleCollider col;
-    void Start()
-    {
-        ninjaRB = GetComponent<Rigidbody>();
-        col = GetComponent<CapsuleCollider>();
-        controller = GetComponent<CharacterController>();
-    }
+//    public CharacterController controller;
+//    public Transform cam;
+//    public float speed = 5;
+//    public float gravity = 9.81f;
+//    private Rigidbody ninjaRB;
+//    public float turnSmoothTime = 0.1f;
+//    float turnSmoothVelocity;
+//    public float jumpForce = 10f;
+//    public LayerMask groundLayers;
+//    public CapsuleCollider col;
+//    void Start()
+//    {
+//        ninjaRB = GetComponent<Rigidbody>();
+//        col = GetComponent<CapsuleCollider>();
+//        controller = GetComponent<CharacterController>();
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, ninjaRB.velocity.y, vertical).normalized;
-        Vector3 lateraldirection = new Vector3(horizontal, 0.0f, vertical).normalized;
-        //Vector3 movement = new Vector3(horizontal, 0, vertical);
-        //ninjaRB.AddForce(movement * speed);
-        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
-        {
-            ninjaRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            //direction.y = jumpForce;
-            //controller.Move(direction.normalized * speed * Time.deltaTime);
-        }
-        if (lateraldirection.magnitude >= 0.1 && IsGrounded())
-        {
-            //in unity we move on x and z axis
-            float TargetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y + direction.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        float horizontal = Input.GetAxisRaw("Horizontal");
+//        float vertical = Input.GetAxisRaw("Vertical");
+//        Vector3 direction = new Vector3(horizontal, ninjaRB.velocity.y, vertical).normalized;
+//        Vector3 lateraldirection = new Vector3(horizontal, 0.0f, vertical).normalized;
+//        //Vector3 movement = new Vector3(horizontal, 0, vertical);
+//        //ninjaRB.AddForce(movement * speed);
+//        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+//        {
+//            ninjaRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+//            //direction.y = jumpForce;
+//            //controller.Move(direction.normalized * speed * Time.deltaTime);
+//        }
+//        if (lateraldirection.magnitude >= 0.1 && IsGrounded())
+//        {
+//            //in unity we move on x and z axis
+//            float TargetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y + direction.y;
+//            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetAngle, ref turnSmoothVelocity, turnSmoothTime);
+//            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 movDir = Quaternion.Euler(0f, TargetAngle, 0) * Vector3.forward;
-            controller.Move(movDir.normalized * speed * Time.deltaTime);
-            //ninjaRB.AddForce(movDir.normalized * speed, ForceMode.Impulse);
-        }
-        //direction.y -= gravity * Time.deltaTime;
-    }
-    private bool IsGrounded()
-    {
-        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
-    }
-}
+//            Vector3 movDir = Quaternion.Euler(0f, TargetAngle, 0) * Vector3.forward;
+//            controller.Move(movDir.normalized * speed * Time.deltaTime);
+//            //ninjaRB.AddForce(movDir.normalized * speed, ForceMode.Impulse);
+//        }
+//        //direction.y -= gravity * Time.deltaTime;
+//    }
+//    private bool IsGrounded()
+//    {
+//        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+//    }
+//}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Variables
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+
+    private Vector3 moveDirection;
+    private Vector3 velocity; //keep track of gravity and jumping
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+
+    [SerializeField] private float jumpHeight;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    //Referances
+    private CharacterController cont;
+    private Animator anim;
+    public Transform cam;
+    private void Start()
+    {
+        cont = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
+       if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; //grounded -> start applying gravity
+            anim.SetBool("IsJumping", false);
+        }
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        //moveDirection = transform.TransformDirection(moveDirection);
+        Vector3 lateraldirection = new Vector3(horizontal, 0.0f, vertical).normalized;
+        if (isGrounded) //no movement in air
+        {
+            
+
+            if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            {
+                Walk();
+            }
+            else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            {
+                Run();
+            }
+            else if (moveDirection == Vector3.zero)
+            {
+                Idle();
+            }
+
+            moveDirection *= moveSpeed;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+        float TargetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        Vector3 movDir = Quaternion.Euler(0f, TargetAngle, 0) * Vector3.forward;
+        cont.Move(movDir.normalized * Time.deltaTime);
+        //cont.Move(moveDirection * Time.deltaTime); //calcullate grav
+
+        velocity.y += gravity * Time.deltaTime;
+
+        cont.Move(velocity * Time.deltaTime); //apply grav to player
+    }
+
+
+    private void Idle()
+    {
+      
+        anim.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
+    }
+    private void Walk()
+    {
+        Debug.Log("Walk");
+        anim.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+        moveSpeed = walkSpeed;
+    }
+    private void Run()
+    {
+        Debug.Log("Run");
+        anim.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
+        moveSpeed = runSpeed;
+    }
+
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        anim.SetBool("IsJumping", true);
+    }
+
+    
+}
