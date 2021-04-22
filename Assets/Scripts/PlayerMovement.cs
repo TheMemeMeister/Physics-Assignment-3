eec0d743a04986c2af343ceda18a29450432921b
TheMemeMeister <60407375+TheMemeMeister@users.noreply.github.com>
2021-04-22 01:32:29 -0400

    more level design

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -66,7 +66,7 @@ public class PlayerMovement : MonoBehaviour
         }
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
         //moveDirection = transform.TransformDirection(moveDirection);
         Vector3 lateraldirection = new Vector3(horizontal, 0.0f, vertical).normalized;
         if (isGrounded) //no movement in air
